b6b3de6ecd1b55cf0a05a45daa8625e74de261d6
Arber Shabhasa <ashabhasa@gmail.com>
2015-03-13 23:39:55 +0100

    tests
    Add golden master

---
+++ GildedRose/SpikeTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+
+namespace GildedRose
+{
+    [TestFixture]
+    public class SpikeTests
+    {
+       [Test]
+        public void Test()
+        {
+            var goldenMaster = ReadGoldenMaster();
+
+            var actualOutput = ReadActualOutput();
+
+            Assert.AreEqual(goldenMaster, actualOutput);
+        }
+
+        private static string ReadActualOutput()
+        {
+            var sb = new StringWriter();
+            Console.SetOut(sb);
+
+            Program.Main(new[] {""});
+            return sb.ToString();
+        }
+
+        private static String ReadGoldenMaster()
+        {
+            var file = new FileInfo(@"..\..\golden-master.txt");
+            using (var sr = file.OpenText())
+            {
+                return sr.ReadToEnd();
+            }
+        }
+    }
+}
